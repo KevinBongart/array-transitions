d34e9947b17a6c6e99ae491c0152495be92db115
Kevin Bongart <contact@kevinbongart.net>
2016-07-25 11:20:26 -0400

    Rubocop refactor

--- lib/array_transitions.rb
+++ lib/array_transitions.rb
@@ -1,16 +1,15 @@
-require "array_transitions/version"
+# frozen_string_literal: true
+require 'array_transitions/version'
 
 class Array
   def transitions
-    t = self.map.with_index do |e, i|
-      next if self.size == i + 1
+    map.with_index do |e, i|
+      next if size == i + 1
 
       next_element = self[i + 1]
       next if next_element == e
 
       { from: e, to: next_element }
-    end
-
-    t.compact
+    end.compact
   end
 end
